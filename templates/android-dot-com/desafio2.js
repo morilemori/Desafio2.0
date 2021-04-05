0de040f8ce74b181f612af25e5bc07547ff6f505
lemori <leandro.tetsuo.mori@gmail.com>
2021-04-05 15:39:38 -0300

    1 - testando conexão (github - atom) {   1.1 - criação de nova branch
      1.2 - desafio2.js foi alterado, sendo acrescentado um escutador de 
    eventos para click e 'empurrando' para o dataLayer

--- templates/android-dot-com/desafio2.js
+++ templates/android-dot-com/desafio2.js
@@ -14,4 +14,20 @@ logo.addEventListener('click', function(){
           'eventLabel': 'seuLabel'
   });
 
-})
+  console.log("trabalhando com o git hub: aprendendo sobre stage");
+});
+
+var menu = document.querySelectorAll(".mdl-navigation__link-mdl-typography--text-uppercase");
+
+menu.addEventListener('click', function() {
+
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'seuEvento',
+    'eventCategory': 'suaCategoria',
+    'eventAction': 'suaAção',
+    'eventLabel': 'seuLabel'
+  });
+
+  console.log("colocando dados do navigation container pro dataLayer ")
+});
