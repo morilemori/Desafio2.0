d8d44d285e06df1073cbfec9a384ae9ab9fa9e0f
lemori <leandro.tetsuo.mori@gmail.com>
2021-04-05 21:30:37 -0300

    DATALAYER: Header: variaveis e valores atribuidos para{ logo android; container navigation [phone, tablets, wear, tv, auto, one, play]; botão lupa; botão [...]; }

--- templates/android-dot-com/desafio2.js
+++ templates/android-dot-com/desafio2.js
@@ -1,61 +1,152 @@
 //Desafio 2.0
 
-//CAPTURANDO O ELEMENTO HTML USANDO UM ATRIBUTO AO INVÉS DA CLASSE PARA PRATICAR O USO DO DATA ATTRIBUTE:
+//IMAGEM DO LOGO
+//Capturando o elemento html (logo android) atraves da classe da imagem
 var logo = document.querySelector(".android-logo-image");
 
-//ADICIONANDO UM ESCUTADOR NO ELEMENTO DO BOTÃO VIEW SOURCE, QUE AO SER CLICADO IRÁ COLOCAR A VARIÁVEL E O EVENTO NO DATALAYER:
+//Adicionando um escutador de evento na imagem do logo. Ao ser clicado, colocará as variaveis e respectivos valores no datalayer;
 logo.addEventListener('click', function(){
 
      window.dataLayer = window.dataLayer || [];
        window.dataLayer.push({
-          'event': 'seuEvento',
-          'eventCategory': 'suaCategoria',
-          'eventAction': 'suaAção',
-          'eventLabel': 'seuLabel'
+          'event': 'genericEvent',
+          'eventCategory': 'headerLogo',
+          'eventAction': 'clickImg',
+          'eventLabel': 'logo'
   });
+  // teste de funcionalidade
+  console.log("adicionando um escutador de evento ao clicarem no logo,");
+});
+/*FIM DO LOGO*/
 
-  console.log("trabalhando com o git hub: aprendendo sobre stage");
+/*CONTAINER NAVIGATION*/
+var navigation = document.querySelectorAll(".mdl-navigation__link-mdl-typography--text-uppercase");
+var phone = navigation[0];
+var tablets = navigation[1];
+var wear = navigation[2];
+var tv= navigation[3];
+var auto = navigation[4];
+var one = navigation[5];
+var play = navigation[6];
+
+phone.addEventListener('click', function() {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'genericEvent',
+    'eventCategory': 'navigationMenu',
+    'eventAction': 'clickLink',
+    'eventLabel': 'phone'
+  });
+  console.log("colocando dados de container>avigation>phone pro dataLayer");
 });
 
-var menu = document.querySelectorAll(".mdl-navigation__link-mdl-typography--text-uppercase");
+tablets.addEventListener('click', function() {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'genericEvent',
+    'eventCategory': 'navigationMenu',
+    'eventAction': 'clickLink',
+    'eventLabel': 'tablets'
+  });
 
-menu.addEventListener('click', function() {
+  console.log("colocando dados de navigation>tablets pro dataLayer");
+});
+
+wear.addEventListener('click', function() {
 
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
-    'event': 'seuEvento',
-    'eventCategory': 'suaCategoria',
-    'eventAction': 'suaAção',
-    'eventLabel': 'seuLabel'
+    'event': 'genericEvent',
+    'eventCategory': 'navigationMenu',
+    'eventAction': 'clickLink',
+    'eventLabel': 'wear'
   });
 
-  console.log("colocando dados do navigation container pro dataLayer ");
+  console.log("colocando dados de navigation>wear pro dataLayer");
 });
 
+tv.addEventListener('click', function() {
+
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'genericEvent',
+    'eventCategory': 'navigationMenu',
+    'eventAction': 'clickLink',
+    'eventLabel': 'tv'
+  });
+
+  console.log("colocando dados de navigation>tv pro dataLayer");
+});
+
+auto.addEventListener('click', function() {
+
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'genericEvent',
+    'eventCategory': 'navigationMenu',
+    'eventAction': 'clickLink',
+    'eventLabel': 'auto'
+  });
+
+  console.log("colocando dados de navigation>auto pro dataLayer");
+});
+
+one.addEventListener('click', function() {
+
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'genericEvent',
+    'eventCategory': 'navigationMenu',
+    'eventAction': 'clickLink',
+    'eventLabel': 'one'
+  });
+
+  console.log("colocando dados de navigation>one pro dataLayer");
+});
+
+play.addEventListener('click', function() {
+
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'genericEvent',
+    'eventCategory': 'navigationMenu',
+    'eventAction': 'clickLink',
+    'eventLabel': 'play'
+  });
+
+  console.log("colocando dados de navigation>play pro dataLayer");
+});
+
+/* FIM NAVIGATION MENU */
+
+/* BOTÃO LUPA */
 var buscar = document.querySelector(".android-search-box mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right mdl-textfield--full-width");
 menu.addEventListener('click', function() {
 
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
-    'event': 'seuEvento',
-    'eventCategory': 'suaCategoria',
-    'eventAction': 'suaAção',
-    'eventLabel': 'seuLabel'
+    'event': 'genericEvent',
+    'eventCategory': 'headerLupa',
+    'eventAction': 'clickBtn',
+    'eventLabel': 'buscar'
   });
 
-  console.log("colocando dados do icone de busca pro dataLayer");
+  console.log("colocando dados do botão de busca pro dataLayer");
 })
+/* FIM DO BOTÃO LUPA */
 
-var sobre = document.querySelector(".android-more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect");
+/* BOTÃO [...] DO HEADER */
+var mais = document.querySelector(".android-more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect");
 sobre.addEventListener('click', function(){
 
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
-    'event': 'seuEvento',
-    'eventCategory': 'suaCategoria',
-    'eventAction': 'suaAção',
-    'eventLabel': 'seuLabel'
+    'event': 'genericEvent',
+    'eventCategory': 'headerMais',
+    'eventAction': 'clickBtn',
+    'eventLabel': 'mais'
   });
 
-  console.log("colocando dados do botão pro dataLayer");
+  console.log("colocando dados do botão mais pro dataLayer");
 })
+/* FIM DO BOTÃO [...] */
